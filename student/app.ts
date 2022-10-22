ebd7694d9c02bf6520c4cd6720c3b9fb52a1f06a
dimuthu-spidertracks <dimuthu.wickramanayake@spidertracks.co.nz>
2022-10-22 22:49:39 +0530

    DynamoDB integration

--- student/app.ts
+++ student/app.ts
@@ -1,14 +1,10 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
+import { DynamoDBClient,  ScanCommand, PutItemCommand, GetItemCommand, DeleteItemCommand, UpdateItemCommand } from "@aws-sdk/client-dynamodb";
+import { marshall, unmarshall } from "@aws-sdk/util-dynamodb";
+import { v4 as uuidv4 } from 'uuid';
 
-/**
- *
- * Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
- * @param {Object} event - API Gateway Lambda Proxy Input Format
- *
- * Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
- * @returns {Object} object - API Gateway Lambda Proxy Output Format
- *
- */
+const tableName = process.env.SAMPLE_TABLE;
+const client = new DynamoDBClient({ region: "us-east-1" });
 
 export const lambdaHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
     let results: any;
@@ -28,7 +24,7 @@ export const lambdaHandler = async (event: APIGatewayProxyEvent): Promise<APIGat
                 results = await createStudents(event);
                 break;
             case 'PUT':
-                results = await updateStudents(event.pathParameters.id)
+                results = await updateStudents(event)
                 break;
             case 'DELETE':
                 results = await deleteStudents(event.pathParameters.id)
@@ -57,21 +53,85 @@ export const lambdaHandler = async (event: APIGatewayProxyEvent): Promise<APIGat
 };
 
 const getStudents = async () => {
-    return ['james', 'john'];
+    try {
+        const params = {
+            TableName : tableName
+        };
+        const { Items } = await client.send(new ScanCommand(params));
+
+        return Items;
+    } catch (e) {
+        throw e;
+    }
 }
 
 const getStudent = async (studentId: string) => {
-    return studentId;
+    try {
+        const params = {
+            TableName: tableName,
+            Key: marshall({ id: studentId })
+        };
+
+        const { Item } = await client.send(new GetItemCommand(params));
+
+        return (Item) ? unmarshall(Item) : {};
+    } catch(e) {
+        throw e;
+    }
 }
 
 const createStudents = async (event: APIGatewayProxyEvent) => {
-    return event;
+    try {
+        const student = JSON.parse(event.body);
+        const studentId = uuidv4();
+        student.id = studentId;
+
+        const params = {
+            TableName: tableName,
+            Item: marshall(student || {})
+        };
+
+        return await client.send(new PutItemCommand(params));
+    } catch(e) {
+        throw e;
+    }
 }
 
 const updateStudents = async (event: APIGatewayProxyEvent) => {
-    return event;
+    try {
+        const requestBody = JSON.parse(event.body);
+        const objKeys = Object.keys(requestBody);   
+    
+        const params = {
+          TableName: tableName,
+          Key: marshall({ id: event.pathParameters.id }),
+          UpdateExpression: `SET ${objKeys.map((_, index) => `#key${index} = :value${index}`).join(", ")}`,
+          ExpressionAttributeNames: objKeys.reduce((acc, key, index) => ({
+              ...acc,
+              [`#key${index}`]: key,
+          }), {}),
+          ExpressionAttributeValues: marshall(objKeys.reduce((acc, key, index) => ({
+              ...acc,
+              [`:value${index}`]: requestBody[key],
+          }), {})),
+        };
+    
+        return await client.send(new UpdateItemCommand(params));
+    } catch(e) {
+        console.error(e);
+        throw e;
+    }
 }
 
 const deleteStudents = async (studentId: string) => {
-    return studentId;
+    try {
+        const params = {
+          TableName: tableName,
+          Key: marshall({ id: studentId }),
+        };
+    
+        return await client.send(new DeleteItemCommand(params));
+    } catch(e) {
+        throw e;
+    }
 }
